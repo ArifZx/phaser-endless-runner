af093e951f6b4d9cd57ede432bfdc60b959587d8
Arif Budiman <arif.budiman@ARIFBUDIMAN.local>
2020-03-09 18:32:19 +0700

    pahse 1 game scene

--- src/gameScene.ts
+++ src/gameScene.ts
@@ -1,5 +1,11 @@
 import "phaser";
 export class GameScene extends Phaser.Scene {
+  addedPlatform: number;
+  platformGroup: Phaser.GameObjects.Group;
+  platformPool: Phaser.GameObjects.Group;
+
+  playerJump:number
+
   constructor() {
     super({
       key: "GameScene"
@@ -13,7 +19,27 @@ export class GameScene extends Phaser.Scene {
     this.load.image("spike", "/assets/platformPack_tile043.png");
   }
 
-  create(data): void {}
+  create(data): void {
+    this.addedPlatform = 0;
 
-  update(time: number, delta: number) {}
+    this.platformGroup = this.add.group({
+      removeCallback: platform => {
+        this.platformPool.add(platform);
+      }
+    });
+
+    this.playerJump = 0;
+  }
+
+  update(time: number, delta: number) {
+    let minDistance  = 800;
+    let rightmostPlatformHeight = 0;
+    this.platformGroup.getChildren().forEach(platform => {
+      let platformDistance = 800 - platform.parentContainer.x - platform.parentContainer.displayWidth / 2;
+      if(platformDistance < minDistance) {
+        minDistance = platformDistance;
+        rightmostPlatformHeight = platform.parentContainer.y;
+      }
+    }, this)
+  }
 }
